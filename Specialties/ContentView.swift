4346944817004215a2d2dd6e669ad630cddcc022
mrmda28 <mrmda28@gmail.com>
2021-08-04 00:34:15 +0300

    Add groups to specialties

--- Specialties/ContentView.swift
+++ Specialties/ContentView.swift
@@ -8,6 +8,26 @@
 
 import SwiftUI
 
+struct GroupsView: View {
+    @State var groups: [Group] = []
+    
+    let specialty: (Int, String)
+    
+    var body: some View {
+        NavigationView {
+            List(groups, id: \.self) { group in
+                VStack {
+                    Text(group.name)
+                }
+            } .onAppear() {
+                API().getGroups(id: self.specialty.0) { (SpecialtyGroups) in
+                    self.groups = SpecialtyGroups.groups }
+            }
+            .navigationBarTitle(Text(self.specialty.1), displayMode: .inline)
+        }
+    }
+}
+
 struct ContentView: View {
     @State var specialties: [Specialty] = []
     
@@ -15,7 +35,9 @@ struct ContentView: View {
         NavigationView {
             List(specialties, id: \.self) { specialty in
                 VStack {
-                    Text(specialty.name)
+                    NavigationLink(destination: GroupsView(specialty:
+                        (specialty.id, specialty.name)))
+                    { Text(specialty.name) }
                 }
             } .onAppear() {
                 API().getSpecialties { (specialties) in
@@ -27,6 +49,16 @@ struct ContentView: View {
 }
 
 struct Specialty: Decodable, Hashable {
+    let id: Int
+    let name: String
+}
+
+struct SpecialtyGroups: Decodable, Hashable {
+    let name: String
+    let groups: [Group]
+}
+
+struct Group: Decodable, Hashable {
     let id: Int
     let name: String
 }
@@ -49,6 +81,34 @@ class API {
                 
                 DispatchQueue.main.async {
                     completion(specialties)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        .resume()
+    }
+    
+    func getGroups(id: Int, completion: @escaping (SpecialtyGroups) -> ()) {
+        guard let url = URL(string: "http://student.ngknn.ru:8001/api/specialties/\(id)/")
+            else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let specialty = try! decoder.decode(SpecialtyGroups.self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(specialty)
                 }
             } catch {
                 print(error)
