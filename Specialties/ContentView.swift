f8a7e9b9b1b9a77d175bf243c7fda5ce503a23e7
mrmda28 <mrmda28@gmail.com>
2021-08-03 20:43:45 +0300

    Initial commit

---
+++ Specialties/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Specialties
+//
+//  Created by Dmitriy Maslennikov on 03/08/2021.
+//  Copyright © 2021 mrmda28. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
