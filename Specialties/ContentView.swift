3f48daee48c849a7e1cb24fd614f0f9208457f22
mrmda28 <mrmda28@gmail.com>
2021-08-03 21:08:11 +0300

    Add JSON parser of specialties from API
    http://student.ngknn.ru:8001/api/specialties/ - is used to get data of specialties in NGKNN. JSON is decoded and displayed in the list.

--- Specialties/ContentView.swift
+++ Specialties/ContentView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var specialties: [Specialty] = []
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(specialties, id: \.self) { specialty in
+                VStack {
+                    Text(specialty.name)
+                }
+            } .onAppear() {
+                API().getSpecialties { (specialties) in
+                    self.specialties = specialties }
+            }
+        .navigationBarTitle("Специальности", displayMode: .inline)
+        }
+    }
+}
+
+struct Specialty: Decodable, Hashable {
+    let id: Int
+    let name: String
+}
+
+class API {
+    func getSpecialties(completion: @escaping ([Specialty]) -> ()) {
+        guard let url = URL(string: "http://student.ngknn.ru:8001/api/specialties/")
+            else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let specialties = try! JSONDecoder().decode([Specialty].self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(specialties)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        .resume()
     }
 }
 
